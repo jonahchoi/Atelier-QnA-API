3395c7d7e4af8b79d127aace013b50230ebe3ffd
jonahchoi <cjonah227@gmail.com>
2022-11-02 09:49:39 -0700

    Added db connection and create tables

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,25 @@
-module.exports = {}
+const { pool } = require('../db');
+
+let model = {
+  getQuestions: async () => {
+    try{
+      const result = await pool.query('SELECT * FROM test');
+      console.log(result);
+    } catch(err) {
+      console.error(err);
+    }
+  },
+
+  addQuestion: async () => {
+    try{
+      const result = await pool.query('INSERT INTO test (id, name) VALUES ($1, $2)', [2, 'john']);
+      console.log('done');
+    } catch(err) {
+      console.error(err);
+    }
+  }
+}
+
+// model.getQuestions();
+
+module.exports = model;
